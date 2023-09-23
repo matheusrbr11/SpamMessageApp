8981de161965e5d2956e0690e108237954e2ed3f
matheusrbr11 <contato.matheusrbr11@gmail.com>
2023-09-23 17:14:37 -0300

    SpamMessageApp

--- interface.py
+++ interface.py
@@ -9,21 +9,44 @@ def pegartexto():
 
     time.sleep(5)
     while x<z:
-        pg.write(f'{a}, #{x}\n')
+        pg.write(f'{a}\n')
         x+=1
 
 root=Tk()
 
-root.geometry("300x150")
-root.title("MyApp")
+root.geometry("350x150")
+root.title("Spam Message App")
 root.config(bg="lightblue")
 
-Entrada1=Entry(root,)
-Entrada1.place(x=25, y=20)
-Entrada2=Entry(root,)
-Entrada2.place(x=25, y=52)
+def on_enter(e):
+    Entrada1.delete(0, 'end')
+    
+def on_leave(e):
+    e1=Entrada1.get()
+    if e1=='':
+        Entrada1.insert(0,'Your Message')
+
+Entrada1=Entry(root)
+Entrada1.insert(0,"Your Message")
+Entrada1.place(x=25, y=20,width=290)
+Entrada1.bind('<FocusIn>', on_enter)
+Entrada1.bind('<FocusOut>', on_leave)
+
+def on_enter(e):
+    Entrada2.delete(0, 'end')
+    
+def on_leave(e):
+    e2=Entrada2.get()
+    if e2=='':
+        Entrada2.insert(0,'How Many Times?')
+
+Entrada2=Entry(root)
+Entrada2.insert(0,"How Many Times?")
+Entrada2.place(x=25, y=52,width=290)
+Entrada2.bind('<FocusIn>', on_enter)
+Entrada2.bind('<FocusOut>', on_leave)
 
 btn=Button(root, text="Enviar", foreground="white", bg="darkgreen", command=pegartexto)
-btn.place(x=25,y=85, width=164)
+btn.place(x=25,y=85, width=290)
 
 root.mainloop()
